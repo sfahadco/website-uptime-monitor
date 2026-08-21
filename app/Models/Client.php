022ec6a0b22d384f5e488289dd12eda5961676ea
skfahad <skfahadco@gmaol.com>
2026-08-21 16:14:16 +0530

    feat: Added Seed data

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['email'];
+
+    public function websites(): HasMany
+    {
+        return $this->hasMany(Website::class);
+    }
 }
--- app/Models/Website.php
+++ app/Models/Website.php
@@ -2,9 +2,27 @@
 
 namespace App\Models;
 
+use App\Enums\WebsiteStatusEnum;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Website extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['client_id', 'url', 'status'];
+
+    protected function casts(): array
+    {
+        return [
+            'last_checked_at' => 'datetime',
+            'status' => WebsiteStatusEnum::class,
+        ];
+    }
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
 }
